e90022dda3ee6864ee8ace81461a880e25aa54ae
ChannnXY <708812775@qq.com>
2020-01-11 21:00:48 +0800

    feature(contact)完成留言的增删改查和分页

--- pages/contact/index/index.js
+++ pages/contact/index/index.js
@@ -1,4 +1,5 @@
 var util = require('../../../utils/myUtil.js');
+var throttle = util.throttle;
 var app = getApp();
 Page({
 
@@ -9,6 +10,9 @@ Page({
     //是否展示登录按钮
     loginFlag:false,
     keyBoardBottom:0,
+    //联系列表
+    contactList:[],
+    contact:{hasNextPage:true,currentPage:0,text:'正在努力加载中'}
   },
 
   /**
@@ -17,6 +21,7 @@ Page({
   onLoad: function (options) {
     this.initScrollView();
     this.checkLogin();
+    this.getContactList();
   },
 
   /**
@@ -44,6 +49,38 @@ Page({
   },
 
   /*
+  * 初始化留言列表
+  */
+  getContactList:function(){
+    var that = this;
+    if (that.data.contact.hasNextPage){
+      var data = {
+        currentPage: this.data.contact.currentPage,
+        pageSize: 5
+      }
+      wx.showLoading({title: '加载中'})
+      util.ajax('contact/get', data,false, function (res) {
+        wx.hideLoading();
+        //拼接数据和分页
+        var data = res.data.data;
+        var contact = that.data.contact;
+        contact.hasNextPage = data.hasNextPage;
+        contact.currentPage++
+        if(data.hasNextPage){
+          contact.text = '上滑加载下一页数据'
+        }else{
+          contact.text = '这是全部留言啦！'
+        }
+        var contactList = that.data.contactList.concat(data.list);
+        that.setData({
+          contactList,
+          contact
+        })
+      })
+    }
+  },
+
+  /*
   * 检查用户是否授权
   */
   checkLogin:function(){
@@ -56,14 +93,14 @@ Page({
           wx.getUserInfo({
             success: res => {
               var data = res.userInfo;
-              console.log(data);
+              // console.log(data);
               //调用登录接口
               wx.login({
                 success: res => {
                   // console.log(res.code)
                   var data = { code: res.code }
                   util.ajax('users/login', data, false, function (res) {
-                    console.log(res.data.data)
+                    // console.log(res.data.data)
                     that.setData({
                       loginFlag: false,
                       user:res.data.data
@@ -86,6 +123,7 @@ Page({
       }
     })
   },
+
   /**
    * 如果用户没有授权，则拉起授权窗口
    */
@@ -111,7 +149,8 @@ Page({
               user: res.data.data
             })
             wx.showToast({
-              title: '雷猴啊！'+res.data.data.nickName+'是你要给孩子一份工作吗',
+              icon:'none',
+              title: '雷猴啊！'+res.data.data.nickName+'！是你要给孩子一份工作吗',
             })
           })
         }
@@ -119,6 +158,81 @@ Page({
     }
   },
 
+  /**
+   * 提交表单
+   */
+  input_contact:function(e){
+    this.setData({content:e.detail.value})
+  },
+
+  submit:throttle(function(e){
+    var that = this;
+    var data = {
+      user_id : that.data.user.id,
+      content: that.data.content
+    }
+    if (that.data.content!==0){
+      util.ajax('contact/input', data, true,function (res) {
+        console.log(res.data.data);
+        var time = new Date();
+        var year = time.getFullYear();
+        var month = time.getMonth() + 1;
+        var date = time.getDate();
+        var newContact = {id:res.data.data,
+          user_id:that.data.user.id,
+          content: that.data.content,
+          nickName:that.data.user.nickName,
+          avatarUrl: that.data.user.avatarUrl,
+          time: year + '年' + month + '月' + date + '日',
+          }
+        var contactList = that.data.contactList;
+        contactList.unshift(newContact);
+        that.setData({ 
+          contactList,
+          content:''
+        })
+        wx.showToast({
+          title: '发布成功！',
+        })
+      })
+    }
+  },1000),
+
+  delete_check:function(e){
+    var that = this;
+    var data = {
+      id: e.currentTarget.dataset.id,
+      user_id: that.data.user.id
+    }
+    var idx = e.currentTarget.dataset.idx;
+    wx.showModal({
+      title: '删除确认',
+      content: '您确定要删除这条留言吗QAQ',
+      success:function(res){
+        if (res.confirm) that.delete_contact(data,idx);
+      }
+    })
+  },
+  
+  delete_contact:throttle(function(data,idx){
+    var that = this;
+    util.ajax('contact/delete',data,false,function(res){
+      console.log(res);
+      if(res.data.code==200){
+        var contactList = that.data.contactList;
+        contactList.splice(idx, 1);
+        that.setData({contactList});
+        wx.showToast({
+          title: '删除成功',
+        })
+      }else{
+        wx.showToast({
+          icon:'none',
+          title: '失败，请刷新后再试',
+        })
+      }
+    })
+  },1000),
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -166,6 +280,29 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    var data = {
+      currentPage: 0,
+      pageSize: 5
+    }
+    util.ajax('contact/get', data, false, function (res) {
+      //拼接数据和分页
+      var data = res.data.data;
+      var contact = that.data.contact;
+      contact.hasNextPage = data.hasNextPage;
+      contact.currentPage = 1;
+      if (data.hasNextPage) {
+        contact.text = '上滑加载下一页数据'
+      } else {
+        contact.text = '这是全部留言啦！'
+      }
+      var contactList = data.list;
+      that.setData({
+        contactList,
+        contact
+      })
+      wx.showToast({
+        title: '刷新成功',
+      })
+    })
   }
 })
